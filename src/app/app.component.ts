4ca000aa4e9c52098632eea2879eb9040141c46f
Ankit Kumar <ankit.rk.ca@gmail.com>
2020-12-02 10:39:20 +0530

    Final Angular Template Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-template-forms';
+  clearances = ['admin', 'student', 'teacher'];
+  @ViewChild('formData', {static: true}) signInForm: NgForm;
+  submitted: boolean = false;
+
+  onFormSubmitted(formData: NgForm) {
+    console.log(formData);
+    console.log(this.signInForm);
+    this.submitted = true;
+    setTimeout(() => {
+      this.signInForm.reset();
+    }, 2000);
+  }
 }
